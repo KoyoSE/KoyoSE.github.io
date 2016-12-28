b4774e001da60b1bd2c0b25e0aac6b68d4a37d70
Toshiki Saito <saito@koyo-se.net>
2016-12-28 16:22:47 +0800

    First commit

---
+++ script/myScript.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+
+    var css = {
+        transition: 'all 2s ease-in-out',
+        transform: 'translate(0px, 50px)'
+    };
+
+    $(".slidePanel").css(css);
+
+});
+
+// Smooth scroll
+  $('a[href^="#"]').click(function () {
+        var speed = 800;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+  });
